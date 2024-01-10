e32714bf61aed6164c17ca30bc31ebb495746a21
vhuynh <vhuynh@gmail.com>
2024-01-10 16:50:02 +0700

    contribute form

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import Layout from "../../components/Layout";
 
 import Campaign from "../../ethereum/campaign";
-import { Card } from "semantic-ui-react";
+import { Card, Grid } from "semantic-ui-react";
+import web3 from "../../ethereum/web3";
+import ContributeForm from "../../components/ContributeForm";
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -12,6 +14,7 @@ class CampaignShow extends Component {
     const sumary = await campaign.methods.getSummary().call();
     console.log("summary", sumary);
     return {
+      address,
       minimumContribution: sumary[0],
       balance: sumary[1],
       requestsCount: sumary[2],
@@ -36,6 +39,30 @@ class CampaignShow extends Component {
           "The manager created this campaign and can create requests to withdraw money",
         style: { overflowWrap: "break-word" },
       },
+      {
+        header: Number(minimumContribution),
+        meta: "Minimum Contribution (wei)",
+        description:
+          "You must contribute at least this much wei to become an approver",
+      },
+      {
+        header: Number(requestsCount),
+        meta: "Number of Requests",
+        description:
+          "A request tries to withdraw money from the contract. Requests must be approved by approvers",
+      },
+      {
+        header: Number(approversCount),
+        meta: "Number of Approvers",
+        description:
+          "Number of people who have already donated to this campaign",
+      },
+      {
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Campaign Balance (ether)",
+        description:
+          "The balance is how much money this campaign has left to spend",
+      },
     ];
     return <Card.Group items={items}></Card.Group>;
   }
@@ -43,7 +70,12 @@ class CampaignShow extends Component {
     return (
       <Layout>
         <h3>Campaign Show</h3>
-        {this.renderCards()}
+        <Grid>
+          <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
+          <Grid.Column width={6}>
+            <ContributeForm address={this.props.address}></ContributeForm>
+          </Grid.Column>
+        </Grid>
       </Layout>
     );
   }

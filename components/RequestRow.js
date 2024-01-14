15763c35323c0d8e4ebc40c39c2a1ea4cb577b9e
vhuynh <vhuynh@gmail.com>
2024-01-14 10:08:12 +0700

    improve request component

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -22,8 +22,13 @@ class RequestRow extends Component {
   render() {
     const { Row, Cell } = Table;
     const { id, request, approversCount } = this.props;
+    const readyToFinalize =
+      parseInt(request.approvalCount) > parseInt(approversCount) / 2;
     return (
-      <Row>
+      <Row
+        disabled={request.complete}
+        positive={readyToFinalize && !request.complete}
+      >
         <Cell>{id}</Cell>
         <Cell>{request.description}</Cell>
         <Cell>{web3.utils.fromWei(request.value, "ether")}</Cell>{" "}
@@ -32,18 +37,14 @@ class RequestRow extends Component {
           {parseInt(request.approvalCount)}/{parseInt(approversCount)}
         </Cell>
         <Cell>
-          {request.complete ? (
-            "Yes"
-          ) : (
+          {request.complete ? null : (
             <Button color="green" basic onClick={this.onApprove}>
               Approve
             </Button>
           )}
         </Cell>
         <Cell>
-          {request.complete ? (
-            "Yes"
-          ) : (
+          {request.complete ? null : (
             <Button color="teal" basic onClick={this.onFinalize}>
               Finalize
             </Button>
--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -44,7 +44,9 @@ class RequestIndex extends Component {
         <h3>Requests</h3>
         <Link route={`/campaigns/${this.props.address}/requests/new`}>
           <a>
-            <Button primary>Add Request</Button>
+            <Button primary floated="right" style={{ marginBottom: 10 }}>
+              Add Request
+            </Button>
           </a>
         </Link>
 
@@ -62,6 +64,7 @@ class RequestIndex extends Component {
           </Header>
           <Body>{this.renderRows()}</Body>
         </Table>
+        <div>Found {parseInt(this.props.requestCount)} requests.</div>
       </Layout>
     );
   }
